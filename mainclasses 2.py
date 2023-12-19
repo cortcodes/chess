c565ee599f210f639becf1257f3f8d0ceed99706
Cortland Niccum <cortlandcode@gmail.com>
2023-12-19 13:26:32 -0700

    classes

--- mainclasses 2.py
+++ mainclasses 2.py
@@ -31,6 +31,7 @@ class ChessBoard:
         self.pieces = []
         self.load_pieces()
 
+
     def load_pieces(self):
         black_pieces = [
             ('black-rook.png', (0, 0), 'black'),
@@ -40,27 +41,36 @@ class ChessBoard:
             ('black-king.png', (400, 0), 'black'),
             ('black-bishop.png', (500, 0), 'black'),
             ('black-knight.png', (600, 0), 'black'),
-            ('black-rook.png', (700, 0), 'black')
+            ('black-rook.png', (700, 0), 'black'),
+            ('black-pawn.png', (0, 100), 'black')
         ]
 
         white_pieces = [
-            ('white-rook.png', (0, 700), 'black'),
-            ('white-knight.png', (100, 700), 'black'),
-            ('white-bishop.png', (200, 700), 'black'),
-            ('white-queen.png', (300, 700), 'black'),
-            ('white-king.png', (400, 700), 'black'),
-            ('white-bishop.png', (500, 700), 'black'),
-            ('white-knight.png', (600, 700), 'black'),
-            ('white-rook.png', (700, 700), 'black')
+            ('white-rook.png', (0, 700), 'white'),
+            ('white-knight.png', (100, 700), 'white'),
+            ('white-bishop.png', (200, 700), 'white'),
+            ('white-queen.png', (300, 700), 'white'),
+            ('white-king.png', (400, 700), 'white'),
+            ('white-bishop.png', (500, 700), 'white'),
+            ('white-knight.png', (600, 700), 'white'),
+            ('white-rook.png', (700, 700), 'white'),
+            ('white-pawn.png', (0, 600), 'white')
         ]
 
-        for img, pos, color in black_pieces:
+        for img, pos, color in black_pieces[:8]:
             piece = ChessPiece(f'pieces/{img}', pos, color)
             self.pieces.append(piece)
         
-        for img, pos, color in white_pieces:
+        for img, pos, color in white_pieces[:8]:
             piece = ChessPiece(f'pieces/{img}', pos, color)
             self.pieces.append(piece)
+
+        for i in range(8):
+            black_pawn = ChessPiece(f'pieces/black-pawn.png', (0 + (100 * i), 100), 'black')
+            white_pawn = ChessPiece(f'pieces/white-pawn.png', (0 + (100 * i), 600), 'white')
+            self.pieces.append(black_pawn)
+            self.pieces.append(white_pawn)
+
 
     def draw(self, screen):
         pygame.draw.rect(screen, BACKGROUND, [0, 0, 800, 800])
@@ -96,24 +106,41 @@ class ChessBoard:
             piece.draw(screen)
 
 
+
 # Create a ChessBoard instance
 chessboard = ChessBoard()
 
 running = True
+selected_piece = None
+offset_x, offset_y = 0, 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # Handle other events like mouse clicks to move pieces
-
-    # Game logic
-    # ...
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for piece in chessboard.pieces:
+                if piece.rect.collidepoint(event.pos):
+                    selected_piece = piece
+                    offset_x = piece.rect.x - event.pos[0]
+                    offset_y = piece.rect.y - event.pos[1]
+                    break
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if selected_piece:
+                # Snap piece position to grid
+                selected_piece.rect.x = (selected_piece.rect.x // 100) * 100
+                selected_piece.rect.y = (selected_piece.rect.y // 100) * 100
+                selected_piece = None
+        elif event.type == pygame.MOUSEMOTION:
+            if selected_piece:
+                mouse_x, mouse_y = event.pos
+                selected_piece.rect.x = mouse_x + offset_x
+                selected_piece.rect.y = mouse_y + offset_y
 
     # Render the game
-    screen.fill('white')  # Clear screen
-    chessboard.draw(screen)  # Draw the chessboard and pieces
-
-    pygame.display.flip()  # Update the display
+    screen.fill('white')
+    chessboard.draw(screen)
+    pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
